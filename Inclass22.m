842b84b146680e9a8c6684a91f572c30ab6ecbcf
ssrinivasan4 <31257594+ssrinivasan4@users.noreply.github.com>
2017-12-01 22:03:59 -0600

    Sanjana Srinivasan
    submitting inclass22

--- Inclass22.m
+++ Inclass22.m
@@ -6,5 +6,67 @@
 %transcripton factor are necessary to activate transciption. Comment on your
 %results. 
 
+figure; hold on;
+ku=0;
+n=1;
+gxfunc2 = @(x,kb) kb*x.^n./(1+x.^n)-x;
+for kb=0:0.05:5
+    gxfunc = @(x) gxfunc2(x,kb);
+    for x0 = 0:0.1:3
+        [rt,~,exitflag] = fzero(gxfunc,x0);
+        if exitflag == 1
+            plot(kb,rt,'k.');
+        end
+    end
+end
+hold off;
+xlabel('k_b');ylabel('Fixed points');
+set(gca,'Fontsize',24);
+
+figure; hold on;
+ku=0;
+n=4;
+gxfunc2 = @(x,kb) kb*x.^n./(1+x.^n)-x;
+for kb=0:0.05:5
+    gxfunc = @(x) gxfunc2(x,kb);
+    for x0 = 0:0.1:3
+        [rt,~,exitflag] = fzero(gxfunc,x0);
+        if exitflag == 1
+            plot(kb,rt,'k.');
+        end
+    end
+end
+hold off;
+xlabel('k_b');ylabel('Fixed points');
+set(gca,'Fontsize',24);
+
+figure; hold on;
+ku=0;
+n=8;
+gxfunc2 = @(x,kb) kb*x.^n./(1+x.^n)-x;
+for kb=0:0.05:5
+    gxfunc = @(x) gxfunc2(x,kb);
+    for x0 = 0:0.1:3
+        [rt,~,exitflag] = fzero(gxfunc,x0);
+        if exitflag == 1
+            plot(kb,rt,'k.');
+        end
+    end
+end
+hold off;
+xlabel('k_b');ylabel('Fixed points');
+set(gca,'Fontsize',24);
+
 % 2. Make a similar diagram for the case of an autorepressing gene in the
-% case that 2 copies are need to turn off the gene. 
+% case that 2 copies are need to turn off the gene.
+
+
+figure;hold on;
+ku = 0;
+for kb = 0:0.5:5
+    polycoeff = [1 kb 1 -ku];
+    rts = roots(polycoeff);
+    rts = rts(imag(rts) == 0);
+    plot (kb*ones(length(rts),1),rts,'r.');
+   
+end
